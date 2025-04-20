04c57550a1c23556eb015b2e095eaa401604cae1
SUPIARTINI <supiartini2@gmail.com>
2025-04-20 17:55:03 +0700

    Add files via upload

---
+++ Modul 1/loop-bintangGanjil.py
@@ -0,0 +1,7 @@
+# Menampilkan segitiga bintang ganjil dengan 3 baris
+    
+tinggi = 3
+for i in range(tinggi):
+    bintang = '*' * (2 * i + 1)  # hanya ganjil: 1, 3, 5, ...
+    spasi = ' ' * (tinggi - i - 1)
+    print(spasi + bintang)
